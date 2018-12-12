c691726a277031f05791db25e3e17f1b34044415
Stefan Ionut Craciunescu <ionut.craciunescu@assist.ro>
2018-12-12 17:33:16 +0200

    https://docs.djangoproject.com/en/2.1/intro/tutorial04/

--- polls/admin.py
+++ polls/admin.py
@@ -3,13 +3,25 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'question_text', 'pub_date')
+    list_filter = ('pub_date', 'question_text')
+    search_fields = ['question_text']
+    list_display = ('qid', 'pub_date', 'was_published_recently', 'older_than_30_days')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
 
 
 class PollsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'choice_text', 'votes', 'question_id');
+    list_display = ('id', 'choice_text', 'votes', 'question_id')
 
 
 admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -11,8 +11,21 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def qid(obj):
+        return "%s - %s" % (obj.id, obj.question_text)
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def older_than_30_days(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=30) >= self.pub_date
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    qid.short_description = "Question"
 
 
 class Choice(models.Model):
